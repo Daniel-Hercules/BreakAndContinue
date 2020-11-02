a8a6448b1b35208d0f663879ca377efec28d9a6c
Daniel-Hercules <73712933+Daniel-Hercules@users.noreply.github.com>
2020-11-02 14:36:14 +0000

    Add files via upload

---
+++ BreakAndContinue/BreakAndContinue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace BreakAndContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            // Counter set to count to 10 
+            for (int c = 0; c < 10; c++)
+            {
+                // But will stop when it gets to 5
+                if (c == 5) 
+                {
+                    break;
+                }
+                Console.WriteLine(c);
+            }
+            // Counter set to count to 10 
+            for (int c = 0; c < 10; c++)
+            {
+                // But will stop when it gets to 5
+                // Continue allows us to continue the parent loop
+                if (c == 5)
+                {
+                    Console.WriteLine("Checkpoint Reached!! Counter is at 5");
+                    continue;
+                }
+                Console.WriteLine("Counter is at " + c);
+            }
+        }
+    }
+}
